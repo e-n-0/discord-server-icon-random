0669b89e9572ffd88cd3ad491c6d6c13efa83b32
Flavien Darche <flavien.darche@epita.fr>
2022-06-01 16:36:37 +0200

    Functional random images from channel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token, serverId, channelImageId, maxImages, timeChange } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -8,7 +8,42 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+
+    executeChange();
+
+    setInterval(function() {
+        changeIcon();
+    }, timeChange);
+    
 });
 
 // Login to Discord with your client's token
 client.login(token);
+
+function executeChange() {
+    getMessagesOfChannel(channelImageId, maxImages).then(messages => {
+        const images = messages.filter(message => message.attachments.size > 0);
+        const imageUrls = images.map(image => image.attachments.first().url);
+        console.log(`Found ${imageUrls.length} image urls`);
+
+        const imageUrl = imageUrls[getRandom(0, imageUrls.length)];
+        console.log(imageUrl);
+        changeIcon(imageUrl);
+    });
+}
+
+function getRandom(min, max) {  
+    return Math.floor(
+      Math.random() * (max - min) + min
+    )
+}
+
+function getMessagesOfChannel(channelId, amount = 100){
+    let channel = client.channels.cache.get(channelId);
+    return channel.messages.fetch({ limit: amount });
+}
+
+function changeIcon(url){
+    let guild = client.guilds.cache.get(serverId);
+    guild.setIcon(url, ['Bot change icon']);
+}
