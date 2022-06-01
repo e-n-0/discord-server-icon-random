7f7fed671c3961c4ca3971c07fce059a55b53531
Flavien Darche <flavien1801@protonmail.com>
2022-06-01 17:48:35 +0200

    Linter + readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# Change Discord Server Icon Randomly
+
+This bot will change your Discord server icon randomly from images uploaded in a Discord channel.
+
+## Usage
+
+### Install project
+
+Install all packages used by the project using `npm`.
+
+```sh
+npm install
+```
+
+### Configuration
+
+Please copy the file `config.example.json` to `config.json` and fill in the values.
+
+### Deploy commands
+
+To use commands you need to register them. Use the command:
+```sh
+node deploy-commands.js
+```
+
+### Run the bot
+
+To run the bot:
+```sh
+node .
+```
--- index.js
+++ index.js
@@ -1,61 +1,65 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token, serverId, channelImageId, maxImages, timeChange } = require('./config.json');
+const { Client, Intents } = require("discord.js");
+const {
+  token,
+  serverId,
+  channelImageId,
+  maxImages,
+  timeChange,
+} = require("./config.json");
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+// At the start of the bot
+client.once("ready", () => {
+  console.log("Ready!");
 
+  // Change image at start
+  executeChange();
+
+  // Change image every x minutes (from config)
+  setInterval(function () {
     executeChange();
-
-    setInterval(function() {
-        changeIcon();
-    }, timeChange);
-    
+  }, timeChange);
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'refresh') {
-        await executeChange();
-		await interaction.reply('Image changé!');
-	}
-});
-
-
-// Login to Discord with your client's token
+// Login to Discord
 client.login(token);
 
+// Command interface
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  // Refresh command - Change the image
+  if (commandName === "refresh") {
+    await executeChange();
+    await interaction.reply("Image changé!");
+  }
+});
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getMessagesOfChannel(channelId, amount = 100) {
+  let channel = client.channels.cache.get(channelId);
+  return channel.messages.fetch({ limit: amount });
+}
+
+async function changeIcon(url) {
+  let guild = client.guilds.cache.get(serverId);
+  await guild.setIcon(url, ["Bot change icon"]);
+}
+
 async function executeChange() {
-    const messages = await getMessagesOfChannel(channelImageId, maxImages);
+  const messages = await getMessagesOfChannel(channelImageId, maxImages);
 
-    const images = messages.filter(message => message.attachments.size > 0);
-    const imageUrls = images.map(image => image.attachments.first().url);
-    console.log(`Found ${imageUrls.length} image urls`);
+  const images = messages.filter((message) => message.attachments.size > 0);
+  const imageUrls = images.map((image) => image.attachments.first().url);
+  console.log(`Found ${imageUrls.length} image urls`);
 
-    const imageUrl = imageUrls[getRandom(0, imageUrls.length)];
-    console.log(imageUrl);
-    await changeIcon(imageUrl);
-}
-
-function getRandom(min, max) {  
-    return Math.floor(
-      Math.random() * (max - min) + min
-    )
-}
-
-function getMessagesOfChannel(channelId, amount = 100){
-    let channel = client.channels.cache.get(channelId);
-    return channel.messages.fetch({ limit: amount });
-}
-
-async function changeIcon(url){
-    let guild = client.guilds.cache.get(serverId);
-    await guild.setIcon(url, ['Bot change icon']);
+  const imageUrl = imageUrls[getRandom(0, imageUrls.length)];
+  console.log(imageUrl);
+  await changeIcon(imageUrl);
 }
